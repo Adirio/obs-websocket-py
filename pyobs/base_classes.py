cf5999408571fd7ec1cbc692eb969a8a4944136e
Adrian Orive <adrian.orive.oneca@gmail.com>
2019-09-11 15:11:17 +0200

    Events and request API cleanup
    Closes #4
    
    Signed-off-by: Adrian Orive <adrian.orive.oneca@gmail.com>

--- pyobs/base_classes.py
+++ pyobs/base_classes.py
@@ -14,19 +14,32 @@ class _Base(object):
 
     @property
     def name(self):
+        """Name of the event or request."""
         return self._name
 
-    def input(self, data):
+    def _fill_returns(self, data):
         self._returns = copy(data)
 
 
 class BaseEvent(_Base):
-    def input(self, data):
-        _Base.input(self, data)
-        del self._returns['update-type']
-
     def __repr__(self):
         return "<{} event ({})>".format(self._name, self._returns)
+
+    @classmethod
+    def from_message(cls, data):
+        """
+        Returns a new event instance with the data from a websocket message
+
+        :param data: websocket message data
+        :return: event instance
+        """
+        obj = cls()
+        obj._fill_returns(data)
+        return obj
+
+    def _fill_returns(self, data):
+        del data['update-type']
+        _Base._fill_returns(self, data)
 
 
 class BaseRequest(_Base):
@@ -34,17 +47,6 @@ class BaseRequest(_Base):
         _Base.__init__(self)
         self._params = {}
         self._status = None
-
-    def data(self):
-        payload = copy(self._params)
-        payload.update({'request-type': self._name})
-        return payload
-
-    def input(self, data):
-        _Base.input(self, data)
-        self._status = self._returns['status'] == 'ok'
-        del self._returns['message-id']
-        del self._returns['status']
 
     def __repr__(self):
         if self._status is None:
@@ -55,3 +57,29 @@ class BaseRequest(_Base):
         else:
             return "<{} request ({}) called: failed ({})>".format(
                 self._name, self._params, self._returns)
+
+    @property
+    def payload(self):
+        """
+        Prepares the websocket message.
+
+        :return: prepared websocket message
+        """
+        payload = copy(self._params)
+        payload.update({'request-type': self._name})
+        return payload
+
+    def answer(self, data):
+        """
+        Fill the response answer of a request from a websocket message.
+
+        :param data: websocket message data
+        :return: Nothing
+        """
+        self._fill_returns(data)
+
+    def _fill_returns(self, data):
+        self._status = data['status'] == 'ok'
+        del data['status']
+        del data['message-id']
+        _Base._fill_returns(self, data)
