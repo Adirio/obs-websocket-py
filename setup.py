0a3418addbae22da9c17f9b34006086d34e5e4f8
Adrian Orive <adrian.orive.oneca@gmail.com>
2019-09-11 09:52:12 +0200

    Change version to 0.1
    Signed-off-by: Adrian Orive <adrian.orive.oneca@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from distutils.core import setup
 from setuptools.command.sdist import sdist
 
-version = "0.4"
+version = "0.1"
 
 # Convert README from Markdown to reStructuredText
 description = "Please take a look at README.md"
